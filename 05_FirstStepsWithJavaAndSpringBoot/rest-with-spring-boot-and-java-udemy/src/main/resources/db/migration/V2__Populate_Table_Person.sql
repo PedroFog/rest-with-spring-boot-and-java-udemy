96fc4b643619bf081e95a216fa0daf0356768c02
pedrofogg <pedro.foganholi2@gmail.com>
2024-04-28 16:51:26 -0300

    Content Negotiation configuration and Flyway

---
+++ 05_FirstStepsWithJavaAndSpringBoot/rest-with-spring-boot-and-java-udemy/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,5 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+	(1, 'Assis, São Paulo', 'Pedro', 'Male', 'Foganholi'),
+	(2, 'Assis, São Paulo', 'Carlos', 'Male', 'Goes'),
+	(3, 'Assis, São Paulo', 'Camila', 'Female', 'Abrao');
+
